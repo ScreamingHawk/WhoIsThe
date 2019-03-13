b285029d7e2521c3f71ead382832bab901f080db
Michael Standen <screaminghawk@gmail.com>
2019-03-13 23:03:24 +1300

    Style title suggest

--- client/src/components/TitleSuggest.js
+++ client/src/components/TitleSuggest.js
@@ -9,7 +9,19 @@ const Wrapper = styled.form`
 	justify-content: center;
 	align-items: middle;
 	background-color: ${({ theme }) => theme.background};
+
+	h1 {
+		white-space: nowrap;
+		margin: 0;
+	}
 `
+
+const placeholders = [
+	'coolest guy',
+	'biggest weeb',
+	'person you would date',
+	'rudest dude',
+]
 
 const TitleSuggest = () => {
 	const [title, setTitle] = useState('')
@@ -25,11 +37,14 @@ const TitleSuggest = () => {
 		setTitle('')
 	}
 
+	const placeholder = placeholders[Math.floor(Math.random() * placeholders.length)] + '?'
+
 	return (
 		<Wrapper onSubmit={handleSubmit}>
+			<h1>Who is the ...&nbsp;</h1>
 			<Input
 				value={title}
-				placeholder="Suggest title ...?"
+				placeholder={placeholder}
 				onChange={e => setTitle(e.target.value)} />
 		</Wrapper>
 	)
